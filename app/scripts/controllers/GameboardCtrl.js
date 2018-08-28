a577a6d8f662fab860c8f2daaaca0166550d0321
Zachary Ehren <zachehren@gmail.com>
2018-08-28 15:43:33 -0700

    create submitLetter logic to update puzzle array

--- app/scripts/controllers/GameboardCtrl.js
+++ app/scripts/controllers/GameboardCtrl.js
@@ -1,28 +1,42 @@
-(function(){
-  function GameboardCtrl(WordFactory) {
-    ctrl = this;
+(function() {
+    function GameboardCtrl(WordFactory, $location) {
+      ctrl = this;
+      let challengeWord;
+      let challengeWordArray;
 
-    let challengeWord = localStorage.getItem("challengeWord");
-
-    ctrl.puzzle = [];
-
-    const createPuzzle = function(){
-      for (let i = 0; i <= challengeWord.length; i++) {
-        ctrl.puzzle.push("_");
+      const createPuzzle = function() {
+        challengeWord = localStorage.getItem("challengeWord");
+        challengeWordArray = challengeWord.split("");
+        ctrl.puzzle = [];
+        for (let i = 0; i < challengeWord.length; i++) {
+          ctrl.puzzle.push("_");
+        }
       }
-    }
 
-    createPuzzle();
+      ctrl.newGame = function() {
+        WordFactory.retrieveWord().then(function() {
+          createPuzzle();
+        });
+      };
 
-    this.newGame = function() {
-      WordFactory.retrieveWord();
-    };
+      ctrl.submitLetter = function() {
+          if (challengeWord.includes(ctrl.letter)) {
+            for (let i = 0; i <= challengeWordArray.length; i++) {
+              if (ctrl.letter == challengeWordArray[i]) {
+                ctrl.puzzle.splice(i, 1, ctrl.letter);
+              }
+            }
+          }
+          let frm = document.getElementsByName('guessLetter')[0];
+          frm.reset();
+        }
 
 
-    this.WordFactory = WordFactory;
-  }
+        ctrl.WordFactory = WordFactory;
 
-  angular
-    .module('hangman')
-    .controller('GameboardCtrl', ['WordFactory', GameboardCtrl]);
-})();
+      }
+
+      angular
+        .module('hangman')
+        .controller('GameboardCtrl', ['WordFactory', '$location', GameboardCtrl]);
+    })();
